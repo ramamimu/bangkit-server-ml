764ffcb695aaf960d54a96a5819bf71d5c468865
ramamimu <ahhmaad.blogger@gmail.com>
2023-06-06 16:06:43 +0700

    feat: pool connection to mysql

--- src/mysql-db/mysql-db.service.ts
+++ src/mysql-db/mysql-db.service.ts
@@ -3,26 +3,33 @@ import * as Mysql from 'mysql';
 
 @Injectable()
 export class MysqlDbService {
-  private db: Mysql.Connection;
+  private db: Mysql.Pool;
   constructor() {
-    this.db = Mysql.createConnection({
+    this.db = Mysql.createPool({
+      connectionLimit: 50,
+      connectTimeout: 2000,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     });
 
-    this.db.connect((err) => {
-      if (err) throw err;
-      console.log('Connected!');
-      this.db.query('show tables', (err, result) => {
-        if (err) throw err;
-        console.log(result);
-      });
-    });
+    console.log(this.getQuery('show tables'));
   }
 
-  getDb(): Mysql.Connection {
-    return this.db;
+  async getQuery(query: string): Promise<string> {
+    let data = '';
+    await this.db.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log('Connected!');
+      connection.query(query, (err, result) => {
+        if (err) throw err;
+        data = result;
+        console.log(result);
+      });
+      connection.release();
+    });
+
+    return data;
   }
 }
