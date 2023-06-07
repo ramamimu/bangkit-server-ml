bcdfce794a4357a909f72363c82b1c9047c186a6
ramamimu <ahhmaad.blogger@gmail.com>
2023-06-07 20:42:02 +0700

    feat: photo reference
    feat: redis module in mysql controller

--- src/mysql-db/mysql-db.module.ts
+++ src/mysql-db/mysql-db.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MysqlDbService } from './mysql-db.service';
 import { MysqlDbController } from './mysql-db.controller';
+import { RedisCacheModule } from 'src/redis-cache/redis-cache.module';
 
 @Module({
   controllers: [MysqlDbController],
   providers: [MysqlDbService],
+  imports: [RedisCacheModule],
 })
 export class MysqlDbModule {}
